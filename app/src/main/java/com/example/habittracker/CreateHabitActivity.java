a5636cf55ee323313fb21f8b6f808a29236fdd31
NoelJasik <Noeloskar99@proton.me>
2023-11-26 22:14:46 +0100

    Działa logika ale się nie pokazuje

--- app/src/main/java/com/example/habittracker/CreateHabitActivity.java
+++ app/src/main/java/com/example/habittracker/CreateHabitActivity.java
@@ -2,6 +2,7 @@ package com.example.habittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,13 +25,20 @@ public class CreateHabitActivity extends AppCompatActivity {
         btnCreateHabit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(etHabitAmount.getText().toString().isEmpty() || etHabitName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(CreateHabitActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 String habitName = etHabitName.getText().toString();
                 int habitAmount = Integer.parseInt(etHabitAmount.getText().toString());
+                Intent intent = new Intent();
+                intent.putExtra("habitName", habitName);
+                intent.putExtra("habitAmount", habitAmount);
 
-                Habit habit = new Habit(habitName,habitAmount);
 
-                Toast.makeText(CreateHabitActivity.this,habit.getHabitName()+" "+habit.getHabitAmount(),Toast.LENGTH_SHORT).show();
-//                finish();
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
 
--- app/src/main/java/com/example/habittracker/HomeActivity.java
+++ app/src/main/java/com/example/habittracker/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.habittracker;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -11,7 +12,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -19,10 +23,20 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity  {
 
 
     MaterialToolbar bottomAppBar;
+
+    List<Habit> habitList = new ArrayList<>();
+
+
+
+
+    private static final int REQUEST_CODE_CREATE_HABIT = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +44,7 @@ public class HomeActivity extends AppCompatActivity  {
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         toolbar.setTitle("Habit Tracker");
+
 
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(
@@ -44,7 +59,7 @@ public class HomeActivity extends AppCompatActivity  {
                 if(item.getItemId() == R.id.task)
                 {
                     Intent intent = new Intent(HomeActivity.this,CreateHabitActivity.class);
-                    startActivity(intent);
+                    startActivityForResult(intent, REQUEST_CODE_CREATE_HABIT);
                 }
                 return false;
             }
@@ -54,6 +69,21 @@ public class HomeActivity extends AppCompatActivity  {
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
 
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CREATE_HABIT && resultCode == RESULT_OK) {
+            String habitName = data.getStringExtra("habitName");
+            int habitAmount = data.getIntExtra("habitAmount", 0);
+            Toast.makeText(this, "Habit Name: " + habitName + " Habit Amount: " + habitAmount, Toast.LENGTH_SHORT).show();
+            Habit habit = new Habit(habitName, habitAmount);
+            habitList.add(habit);
+            // Use the values of habitName and habitAmount
+        }
     }
 
 }
